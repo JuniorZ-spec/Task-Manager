93dfb3189f18170db3a88712d141325a55fd2388
DevJun19 <juniorsessi6@gmail.com>
2025-11-17 12:24:11 +0100

    Navbar

---
+++ frontend/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { Zap } from 'lucide-react';
+
+function Navbar() {
+
+    const navigate = useNavigate();
+
+    return (
+        <header className=' sticky top-0 z-50 bg-white/90  backdrop-blur-md shadow-sm border-b border-gray-200 font-sans   '>
+
+
+            <div className=' flex items-center justify-between px-4 py-3 md:px-6 max-w-7xl  mx-auto'>
+
+
+                <div className=' flex items-center gap-2 cursor-pointer group'
+                    onClick={() => navigate('/')}>
+
+                    { /* LOGO */}
+                    <div className='relative w-10 h-10 flex items-center justify-center rounded-xl bg-gradient-to-br from-fuchsia-600 via-purple-600 to-indigo-500 
+                    shadow-lg  group-hover:shadow-purple-300/50'>
+
+                    </div>
+
+
+                </div>
+
+
+            </div>
+
+        </header>
+    )
+}
+
+export default Navbar
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Dashboard from '../src/pages/Dashboard'
 
 const App = () => {
   return (
-    <div>
-
-      <h1 className="text-3xl font-bold underline"> Hello World</h1>
+    <>
 
 
+      <Navbar />
+      <Dashboard />
 
-    </div>
+
+
+    </>
   )
 }
 
